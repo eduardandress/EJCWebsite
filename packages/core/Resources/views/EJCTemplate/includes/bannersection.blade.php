72b263722c5dd06901f4d7fa79f3730154a0e0ff
Eduardo Carvallo <edu1455@gmail.com>
2016-10-26 09:28:58 -0430

    Cambios en el banner principal

--- packages/core/Resources/views/EJCTemplate/includes/bannersection.blade.php
+++ packages/core/Resources/views/EJCTemplate/includes/bannersection.blade.php
@@ -6,26 +6,43 @@
       padding:  15px;
    }
 </style>    
-<div class="section no-pad-bot demoBgn  demoback1 valign-wrapper" id="index-banner">
+<div class="section no-pad-bot demoBgn  blue darken-4 valign-wrapper" id="index-banner">
   
 
     
     <!-- <h1 class="header center white-text">EJC Software Solutions</h1> -->
    
-   <div style="display:flex; justify-content: space-between; margin: 0 10%;">
-     <div style="width: 60%">
-       <p class="header  light white-text main-text-panel  teal lighten-1">We're a Small Business dedicated to the development of <strong> Web and Mobile Applications </strong> using the best tools, the best programming practices, and computational techniques. <br> Giving you software solutions to the second power!</p>
+ <div class="center-align" style="display:flex; flex-flow: row wrap; justify-content: space-between; margin: 0 10%; align-items: center">
+    <div style="width: 40%; flex-grow: 1">
+        <h1 class="white-text center-align">
+          Web and Mobile Applications Development
+        </h1>
+         <div class="card  light white-text teal lighten-1">
+            <div class="card-content">
+              <p class="header text-center">
+                 using the best tools, the best programming practices, and computational techniques. <br> Giving you software solutions to the second power!
+              </p>
+            </div>
+         </div>
+        <a href="#servicesSection" id="download-button" class="btn-large center waves-effect waves-light light-blue darken-2 goTo">Request our services</a>
 
-     </div>
-   </div>
-    
-     <div class="container valign"> 
-
-    
-    <div class="row center">
-      <a href="#servicesSection" id="download-button" class="btn-large waves-effect waves-light light-blue darken-2 goTo">Request our services</a>
     </div>
-    <br><br>
+
+    <div style="margin-left: 20px">
+      
+      <img  class="img-responsive" style="width:100%;" src="{{URL::asset('assets/images/main-banner-img.png')}}" alt="">
+    </div>
 
   </div>
+ 
+    
+   <!--  <div class="container valign"> 
+   
+   
+   <div class="row center">
+     <a href="#servicesSection" id="download-button" class="btn-large waves-effect waves-light light-blue darken-2 goTo">Request our services</a>
+   </div>
+   <br><br>
+
+  </div> -->
 </div>
